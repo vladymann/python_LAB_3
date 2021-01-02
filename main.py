a8c3c146aed1dc34ffece70cfcd9cb36c32e6d2e
Vlad <76779641+vladymann@users.noreply.github.com>
2021-01-02 14:51:18 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+name="idan hakimi"
+age=28
+mail="idan@gmail.com"
+print("full name: " + name + "\nage: " + str(age) + "\nmail: " + mail)
+print("\n\nfull name: " + name[::-1] + "\nage: " + str(age*3))
+print("idan" in "yana nana idan boris josef")
+print("elia" in "yana nana idan boris josef")
